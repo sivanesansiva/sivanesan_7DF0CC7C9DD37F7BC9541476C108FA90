683a8a9bb024ee47f42605ae97cbaf50230712c5
sivanesansiva <145024639+sivanesansiva@users.noreply.github.com>
2023-09-14 04:26:26 -0900

    Add files via upload

---
+++ main (2) - Copy.py
@@ -0,0 +1,19 @@
+# leep year
+""""""
+("year %4==0")
+("year %100!=0")
+("year %400==0")
+""""""
+"def_isleapyear"
+if ("year%4==0'and'year%100!=0'or'year%400==0"):
+  "return" " true"
+else:
+  "return" "False"
+  year = 2012
+
+if "isaleapyear":
+  print ('{}isaleapyear',
+format('year'))
+else:
+  print('{}isaleapyear',
+format('year'))
